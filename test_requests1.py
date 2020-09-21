486fdd4695124bf16b0d9b472c5e744597279dae
xiaocaiji945 <412278432@qq.com>
2020-09-21 19:01:45 +0800

    base64

--- test_requests1.py
+++ test_requests1.py
@@ -10,5 +10,7 @@ class ApiRequests:
         res = requests.request(data["method"], data["url"], headers=data["headers"])
         if data["encoding"] == "base64":
             return json.loads(base64.b64decode(res.content))
+
+        # 把加密后的响应值发送给第三方服务，让第三方去做解密然后返回解密过后的信息
         elif data["encoding"] == "private":
             return requests.post("url", data=res.content)
